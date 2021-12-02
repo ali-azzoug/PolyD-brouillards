47a11549b0e86dd65768f37c51ed372119609969
Cazeq <emailenplus2@gmail.com>
2021-12-02 14:25:11 +0100

    Gestion des vidéos

--- nodejs-express-mongodb/app/controllers/playlist.controller.js
+++ nodejs-express-mongodb/app/controllers/playlist.controller.js
@@ -1,25 +1,23 @@
 const db = require("../models");
 const jwt = require("jsonwebtoken");
 const config = require("../config/auth.config.js");
+const { json } = require("body-parser");
 const Playlist = db.playlists;
 
 
 // Create and Save a new playlist
 exports.create = (req, res) => {
   
-    //let token = req.headers["x-access-token"];
-    //const decodedToken = jwt.verify(token, config.secret);
-    
     // Validate request
   if (!req.body.name) {
     res.status(400).send({ message: "Content can not be empty!" });
     return;
   }
 
-  // Create a Tutorial
+  // Create a playlist
   const playlist = new Playlist({
     name: req.body.name,
-    video_list: null,
+    video_list: [],
     created_by: req.body.createdBy,
     updated: new Date,
   });
@@ -50,4 +48,67 @@ exports.FindPlaylistsByUsername = (req, res) => {
             err.message || "Some error occurred while retrieving playlists."
         });
       });
+  };
+
+
+  // Update a playlist by adding a video in the request
+exports.addVideo = (req, res) => {
+  if (!req.body) {
+    return res.status(400).send({
+      message: "Data to update can not be empty!"
+    });
+  }
+  const myObject = {videoId:"001",titre:"best video",description:"test"};
+  const id = req.body.id
+
+  Playlist.findByIdAndUpdate(id, {"$push" :{"video_list" : myObject }})
+    .then(data => {
+      if (!data)
+        res.status(404).send({ message: "Not found playlist with id " + id });
+      else res.send(data);
+    })
+    .catch(err => {
+      res
+        .status(500)
+        .send({ message: "Error retrieving playlist with id=" + id });
+    });
+  
+};
+
+  // GET 1 playlist
+  exports.getOnePlaylist = (req, res) => {
+  
+    const id = req.body.id;
+  
+    Playlist.findById(id)
+      .then(data => {
+        if (!data)
+          res.status(404).send({ message: "Not found playlist with id " + id });
+        else res.send(data);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .send({ message: "Error retrieving playlist with id=" + id });
+      });
+    
+  };
+
+   // Update a playlist by adding a video in the request
+   exports.getAllVideoFromPlaylist = (req, res) => {
+  
+    const id = req.body.id;
+
+    Playlist.findById(id)
+      .then(data => {
+        if (!data)
+          res.status(404).send({ message: "Not found playlist with id " + id });
+        else res.send(data.video_list);
+      })
+      .catch(err => {
+        res
+          .status(500)
+          .send({ message: "Error retrieving playlist with id=" + id });
+      });
+    
   };
--- nodejs-express-mongodb/app/routes/playlist.routes.js
+++ nodejs-express-mongodb/app/routes/playlist.routes.js
@@ -9,5 +9,12 @@ module.exports = app => {
     // Retrieve all playlists by username
     router.post("/findPlaylists", playlists.FindPlaylistsByUsername);
 
+  // Retrieve all playlists by username
+    router.post("/addVideo/", playlists.addVideo);
+
+    router.post("/getOnePlaylist/", playlists.getOnePlaylist);
+
+    router.post("/getAllVideoFromPlaylist/", playlists.getAllVideoFromPlaylist);
+
     app.use('/api/playlists', router);
   };
