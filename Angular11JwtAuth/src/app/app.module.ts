fcae339cc0be149de62fd9e89f673cc698a62d2d
Cazeq <emailenplus2@gmail.com>
2021-12-20 21:38:50 +0100

    Ajout de design

--- Angular11JwtAuth/src/app/app.module.ts
+++ Angular11JwtAuth/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { BoardUserComponent } from './board-user/board-user.component';
 import { BoardAnnonceurComponent } from './board-annonceur/board-annonceur.component';
 
 import { authInterceptorProviders } from './_helpers/auth.interceptor';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
@@ -32,7 +33,8 @@ import { authInterceptorProviders } from './_helpers/auth.interceptor';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule,
   ],
   providers: [authInterceptorProviders],
   bootstrap: [AppComponent]
--- Angular11JwtAuth/src/app/home/home.component.ts
+++ Angular11JwtAuth/src/app/home/home.component.ts
@@ -3,6 +3,10 @@ import { UserService } from '../_services/user.service';
 import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { YoutubeapiService } from '../_services/youtubeapi.service';
 import { TokenStorageService } from '../_services/token-storage.service';
+import { AnnonceService } from '../_services/annonce.service';
+import { PlaylistService } from '../_services/playlist.service';
+import {ModalDismissReasons, NgbModal} from '@ng-bootstrap/ng-bootstrap';
+
 
 @Component({
   selector: 'app-home',
@@ -11,6 +15,8 @@ import { TokenStorageService } from '../_services/token-storage.service';
 })
 export class HomeComponent implements OnInit {
   isLoggedIn: boolean = false ;
+  closeResult: string = "";
+  currentUser = this.tokenStorageService.getUser();
   VideoQuery = "";
   youtubeData = {items: [
     {
@@ -32,15 +38,21 @@ export class HomeComponent implements OnInit {
   currentVideoTitle: string | undefined;
   currentVideoDescription: string | undefined;
 
+  MyPlaylists : any;
+  currentPlaylistName:string="";
   currentPlaylist: Array<{videoId: string, title:string, description:string}> = []
   urlSafe: SafeResourceUrl | undefined; 
 
+  errorMessage= "";
 
   constructor(
     private userService: UserService, 
+    private playlistService: PlaylistService,
     private youtubeapi: YoutubeapiService,
     public sanitizer: DomSanitizer,
     private tokenStorageService: TokenStorageService,
+    private annonceService: AnnonceService,
+    private modalService: NgbModal
     ) { }
 
   ngOnInit(): void {
@@ -48,6 +60,14 @@ export class HomeComponent implements OnInit {
     this.youtubeData = this.youtubeapi.queryYoutube("populaire", 9);
     this.listeVideos = this.youtubeData.items;
     this.isLoggedIn = !!this.tokenStorageService.getToken();
+    this.annonceService.getAnnonce("").subscribe(
+      data => {
+        console.log(data[0].budget);
+      },
+      err => {
+        console.log(JSON.parse(err.error).message);
+      }
+    );
 
   }
 
@@ -91,14 +111,68 @@ export class HomeComponent implements OnInit {
 
     this.currentPlaylist.push({videoId,title,description})
 
-    
+  }
+
+  addVideoToPlaylist(Playlist: any, videoId: string, title: string, description: string) {
+
+    const data = {
+      idPlaylist: Playlist._id,
+      videoId:videoId,
+      titre: title,
+      description:description
+    };
+
+    this.currentPlaylistName = Playlist.name;
+    this.playlistService.addVideoToPlaylist(data).subscribe(
+      data => {
+        console.log(data);
+      },
+      err => {
+        this.errorMessage = err.error.message;
+      }
+    );
 
   }
+
   removeFromPlaylist(video:{videoId: string, title: string, description: string}) {
     for(let i =0; i<this.currentPlaylist.length; i++){
       if(this.currentPlaylist[i] === video){
         this.currentPlaylist.splice(i, 1);
       }
+    }
+  }
+
+  open(content:any) {
+
+    const data = {
+      createdBy: this.currentUser.username,
+    };
+
+    // console.log(data)
+    this.playlistService.findByUsername(data).subscribe(
+      data => {
+        console.log(data);
+        this.MyPlaylists = data;
+      },
+      err => {
+        this.errorMessage = err.error.message;
+      }
+    );
+
+    this.modalService.open(content, {ariaLabelledBy: 'modal-basic-title'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+  
+  private getDismissReason(reason: any): string {
+    if (reason === ModalDismissReasons.ESC) {
+      return 'by pressing ESC';
+    } else if (reason === ModalDismissReasons.BACKDROP_CLICK) {
+      return 'by clicking on a backdrop';
+    } else {
+      return `with: ${reason}`;
     }
   }
 
